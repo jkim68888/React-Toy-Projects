d16708f26ee0f05132fa101e099de9cb00c1d2bf
Jihyun <jkim68888@gmail.com>
2021-04-20 17:54:26 +0900

    우정테스트 화면 레이아웃 작업중

---
+++ 2021-04-20-friendship-test/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import FriendshipTest from './Friendship-test';
+
+function App() {
+  return (
+    <div className="App">
+      <FriendshipTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2021-04-20-friendship-test/src/Friendship-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './person.png';
+
+const FriendshipTest = () => {
+    return(
+        <div className="friendship-test-wrap">
+            <img src={logo} alt="로고"/>
+        </div>
+    );
+};
+
+export default FriendshipTest;
