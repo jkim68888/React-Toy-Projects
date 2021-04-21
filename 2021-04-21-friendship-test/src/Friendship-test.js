fd1968a65593d9c2497cdd9af7d3bb17300e12b4
Jihyun <jkim68888@gmail.com>
2021-04-21 17:12:06 +0900

    버그 수정

--- 2021-04-21-friendship-test/src/Friendship-test.js
+++ 2021-04-21-friendship-test/src/Friendship-test.js
@@ -4,23 +4,20 @@ const FriendshipTest = () => {
     const [name, setValues] = useState({name: ''});
 
     const handleInputChange = (e) => {
-        const my_name = document.getElementById('my-name');
-
         setValues({
             ...name,
             [e.target.name]: e.target.value
         }); 
 
         console.log(name); /* {name: "지현"} */
-        console.log(name.value); /* undefined */
-        my_name.textContent = {name}; /* [object Object] */
+        console.log(name.name);
     }
 
     return(
         <div className="friendship-test-wrap">
             <form>
-                <h2>나는 <span id="my-name">"  "</span> 에 대해 얼마나 알고있을까?</h2>
-                <input type="text" placeholder="이름" name="name" onFocus={handleInputChange} onChange={handleInputChange}/>
+                <h2>나는 <span id="my-name">" {name.name} "</span> 에 대해 얼마나 알고있을까?</h2>
+                <input type="text" placeholder="이름" name="name" onChange={handleInputChange}/>
                 <button type="submit">GO</button>
             </form>
         </div>
