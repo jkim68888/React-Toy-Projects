baf9f9a010c36043a26baafb64c84aabb5d225a8
Jihyun <jkim68888@gmail.com>
2021-04-21 10:30:08 +0900

    폴더명 변경

--- 2021-04-21-friendship-test/src/App.js
+++ 2021-04-21-friendship-test/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './style.css';
+import logo from './person.png';
 import FriendshipTest from './Friendship-test';
 
 function App() {
   return (
     <div className="App">
-      <FriendshipTest />
+      <div className="App-wrapper">
+        <img src={logo} alt="로고"/>
+        <FriendshipTest />
+      </div>
     </div>
   );
 }
--- 2021-04-21-friendship-test/src/Friendship-test.js
+++ 2021-04-21-friendship-test/src/Friendship-test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './person.png';
 
 const FriendshipTest = () => {
     return(
         <div className="friendship-test-wrap">
-            <img src={logo} alt="로고"/>
+            
         </div>
     );
 };
