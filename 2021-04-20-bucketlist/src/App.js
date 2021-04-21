e2ae8758949a83c8aaed2e629f709362bf759734
Jihyun <jkim68888@gmail.com>
2021-04-22 03:36:27 +0900

    input 박스 추가

--- 2021-04-20-bucketlist/src/App.js
+++ 2021-04-20-bucketlist/src/App.js
@@ -9,6 +9,13 @@ class App extends React.Component {
     this.state = {
       list: ['영화관 가기', '매일 책읽기', '수영 배우기','자전거 타기'],
     };
+
+    this.text = React.createRef();
+  }
+
+  componentDidMount(){
+    console.log(this.text);
+    console.log(this.text.current); /* ref 는 current 로 가져옴 - console 찍어보면 확인가능 */
   }
 
   render() {
@@ -18,6 +25,9 @@ class App extends React.Component {
             <h1 className="title">내 버킷리스트</h1>
             <hr className="line"/>
             <BucketList list={this.state.list} />
+        </div>
+        <div>
+          <input type="text" ref={this.text}/>
         </div>
       </div>
     );
