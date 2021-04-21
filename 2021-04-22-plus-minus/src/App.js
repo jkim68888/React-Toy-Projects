cb78b8fd78c94a491b9fa1bf0bdbe5ec8eb63f6f
Jihyun <jkim68888@gmail.com>
2021-04-22 05:14:29 +0900

    네모 추가, 삭제 함수 구현

--- 2021-04-22-plus-minus/src/App.js
+++ 2021-04-22-plus-minus/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 1,
+    }
+  }
+
+
+  plusNemo = () => {
+    const alert = document.getElementById('alert');
+
+    this.setState({count: this.state.count + 1});
+    
+    if(this.state.count >= 0){
+      alert.style.display = 'none';
+    }else{
+      alert.style.display = 'inline';
+    }
+
+  }
+
+  minusNemo = () => {
+    const alert = document.getElementById('alert');
+
+    if(this.state.count > 0){
+      this.setState({count: this.state.count - 1});
+    }else{
+      alert.style.display = 'inline';
+    }
+  }
+
+  render(){
+    const nemo_count = Array.from({length: this.state.count});
+
+    return(
+      <div className="App"> 
+        <h2>네모를 plus , minus 해봐요💕</h2>
+        <div className="nemo-wrap">
+          <p id="alert">네모가 없어요ㅠㅠ Plus 해주세요!</p>
+          {
+            nemo_count.map((num, index) => {
+              return(
+                <div key={index} className="nemo">네모네모~</div>
+              )
+            })
+          }
+        </div>
+        <button onClick={this.plusNemo} type="button" className="plus">PLUS</button>
+        <button onClick={this.minusNemo} type="button" className="minus">MINUS</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
