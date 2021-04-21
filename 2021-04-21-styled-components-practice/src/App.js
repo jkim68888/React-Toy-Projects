0431b755037ae37fe13b9d1e5f972f4a7ff0ef1a
Jihyun <jkim68888@gmail.com>
2021-04-21 21:03:10 +0900

    styled components 적용 연습

--- 2021-04-21-styled-components-practice/src/App.js
+++ 2021-04-21-styled-components-practice/src/App.js
@@ -1,25 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyStyled bgColor={''}>Hello React!</MyStyled>
     </div>
   );
 }
+
+const MyStyled = styled.div`
+  width: 50vw;
+  min-height: 150px;
+  padding: 10px;
+  border-radius: 15px;
+  color: #fff;
+  &:hover{
+    background-color: #ddd;
+  }
+  background-color: ${(props) => (props.bgColor? 'red' : 'purple')};
+`;
 
 export default App;
