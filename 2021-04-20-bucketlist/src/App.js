e45c6774f214ca531a596f1fd906ded865baddce
Jihyun <jkim68888@gmail.com>
2021-04-24 22:23:30 +0900

    리스트 추가 기능 구현

--- 2021-04-20-bucketlist/src/App.js
+++ 2021-04-20-bucketlist/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import BucketList from './BucketList';
-import AddList from './AddList';
 import './style.scss';
 
 class App extends React.Component {
@@ -10,6 +9,14 @@ class App extends React.Component {
     this.state = {
       list: ['영화관 가기', '자전거 타기'],
     };
+    this.text = React.createRef();
+  }
+
+  addList = () => {
+    let list = this.state.list;
+    const new_item = this.text.current.value;
+
+    this.setState({list: [...list, new_item]});
   }
 
   render() {
@@ -19,7 +26,11 @@ class App extends React.Component {
             <h1 className="title">내 버킷리스트</h1>
             <hr className="line"/>
             <BucketList list={this.state.list} />
-            <AddList/>
+            <div className="add-list-wrap">
+                <h3>버킷리스트를 추가해주세요💖</h3>
+                <input type="text" ref={this.text}/>
+                <button type="submit" onClick={this.addList}>추가</button>
+            </div>
         </div>
       </div>
     );
