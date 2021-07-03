10b6cff4aabf3572996703df2872c78566d3d666
jkim68888 <jkim68888@gmail.com>
2021-07-03 10:50:02 +0900

    컴포넌트 작업중

--- 2021-06-29-todo-list/src/App.js
+++ 2021-06-29-todo-list/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -12,7 +15,11 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <TodoTemplate>안녕하세요!</TodoTemplate>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
     </>
   );
 }
